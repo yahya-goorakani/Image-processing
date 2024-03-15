034b26451816c751c9bea3a8370f88224761fd27
yahya <115286845+yahya-goorakani@users.noreply.github.com>
2024-03-15 11:41:55 +0000

    .

---
+++ unsharpMask.py
@@ -0,0 +1,14 @@
+import cv2 as cv 
+def unshpar():
+    image = cv.imread('img.jpg')
+    if image is None:
+      print(" Error:Enable to load file. ")
+    else:
+      gaussian = cv.GaussianBlur(image, (0, 0), 2.0)
+      return cv.imwrite('unshapr_image.jpg',cv.addWeighted(image, 2.2, gaussian, -1.0, 0))
+
+if __name__ == "__main__":
+      
+    unshpar()
+    
+    
